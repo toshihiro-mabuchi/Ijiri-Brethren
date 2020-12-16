22084941c9f1cea6e4feecc032fbccf5b9f4b089
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2020-12-16 22:20:08 +0900

    ログイン画面

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,7 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-
   ### 上記と同様の処理を別の記載方法で書いた場合 ###
-
   # before do
   #   @user = User.new(
   #     name: "test",
@@ -33,9 +31,7 @@ RSpec.describe User, type: :model do
   # it { should respond_to(:password_confirmation) }
   # it { should be_valid }
 
-
   ### パスワードのチェックを個別に書いた場合 ###
-  
   # it 'パスワードに英小文字が含まれない場合無効な状態であること' do
   #   user = User.new(password: '1'+'A' * 5, password_confirmation: '1'+'A' * 5)
   #   user.valid?
@@ -65,7 +61,6 @@ RSpec.describe User, type: :model do
   #   user.valid?
   #   expect(user.errors[:password]).to include('は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります')
   # end
-
   #-------------------------------------------------#
 
 end
