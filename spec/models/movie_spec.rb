d69a8a82767774386373df18b061ec7d104d09c1
topazon <topazonzon@gmail.com>
2020-12-28 15:38:00 +0900

    model_spec変更

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,18 +3,19 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 
-  it '動画タイトルと動画URLがあれば有効な状態であること' do
+  it 'タイトルと説明文、サムネイル、youtubeID、動画URL、
+      動画投稿主、カテゴリーがあれば有効な状態であること' do
     movie = Movie.new(
         title: "aaa",
+        text: "こんにちは　こんにちは　こんにちは",
+        thumbnail_url: "https://youtu.be/G413S5OqSVA",
         youtube_mid: "fEvM-OUbaKs",
         youtube_url: "https://youtu.be/fEvM-OUbaKs",
-        author_name: "kobayashi"
+        author_name: "kobayashi",
+        category: "メンバー"
       )
     expect(movie).to be_valid
   end
 
-  it '動画URLがあれば有効な状態であること'
-  #   expect().to be_valid
-  # end
 
 end
