76b66e6caa6b9a938f569da725a3c347dadc4f9d
topazon <topazonzon@gmail.com>
2020-12-28 21:56:46 +0900

    infoルート追加

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,2 +1,26 @@
 class InfosController < ApplicationController
+
+  def index
+    @infos = Info.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def strong_info
+
+    end
 end
