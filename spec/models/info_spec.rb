f990a284f142e368d79b8b7fa2ded3ba5e1f63bd
topazon <topazonzon@gmail.com>
2021-01-12 00:06:35 +0900

    モデルスペック作成

--- spec/models/info_spec.rb
+++ spec/models/info_spec.rb
@@ -4,18 +4,23 @@ RSpec.describe Info, type: :model do
   
   before do
     @info_presence = Info.new(
-      titke: "お知らせ",
+      title: "お知らせ",
       content: "お知らせの本文--------------------"
     )
   end
 
   it 'タイトルと本文があれば有効な状態であること' do
-    expect(@info).to be_valid
+    expect(@info_presence).to be_valid
   end
 
-  it "ユーザー名が５１文字以上の場合は登録できない" do
-    user = User.new(name: "a" * 51, password: "password")
-    expect(user).to be_invalid
+  it "タイトルが５１文字以上の場合は登録できない" do
+    info = Info.new(title: "a" * 51)
+    expect(info).to be_invalid
+  end
+
+  it "本文が５００文字以上の場合は登録できない" do
+    info = Info.new(content: "a" * 500)
+    expect(info).to be_invalid
   end
 
 end
