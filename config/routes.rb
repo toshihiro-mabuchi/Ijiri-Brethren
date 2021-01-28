f39f0f66bca6c9d50590333686404d271194af02
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2021-01-29 03:07:34 +0900

    member-page-init

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :infos
 
-  resources :users, only: [:index, :destroy]
+  resources :users, only: [:index, :show, :destroy]
 
   resources :movies do
     collection do
