c0af9eaac9432b8a554dd0c17e913ba165f11eac
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2021-03-12 21:26:59 +0900

    member_page_update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   # layout 'admin_page', except: [:members_view, :general_view]
-  before_action :logged_in_user, only: %i(index show)
-  before_action :admin_user, only: %i(new create edit update destroy)
+  before_action :logged_in_user, only: %i(index show members_view general_view)
+  before_action :admin_user, only: %i(index new create edit update destroy general_view)
   before_action :set_member_movies, only: %i(index create update destroy)
   before_action :set_general_movies, only: %i(index create update destroy)
 
