50ed6d84ba8d17317c7c86425fe4d1afab4aa05e
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2021-03-30 23:58:26 +0900

    User_model_edit

--- spec/requests/admin_pages_request_spec.rb
+++ spec/requests/admin_pages_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "AdminPages", type: :request do
+  pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "GET /admin_pages" do
-    it "returns http success" do
-      get admin_pages_path
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /admin_pages" do
+  #   it "returns http success" do
+  #     get admin_pages_path
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
--- spec/requests/infos_request_spec.rb
+++ spec/requests/infos_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Infos", type: :request do
+  pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/infos/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /index" do
+  #   it "returns http success" do
+  #     get "/infos/index"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
